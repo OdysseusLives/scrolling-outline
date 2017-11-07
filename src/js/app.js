9ad0aaf715470d96d2c31a5df647aea33fa33ae0
Aubrey Chipman <aubrey.chipman@gmail.com>
2017-11-07 01:30:05 +0100

    Mermaid css scoping is too large. Remove pagepiling.js until PR 577 is merged into Mermaid.

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,30 @@
 'use strict';
 
-var pagepiling = require('pagepiling.js');
+// var pagepiling = require('pagepiling.js');
 var mermaid = require('mermaid');
 
-mermaid.initialize({startOnLoad:true});
+$(document).load(function() {
+    mermaid.initialize();
+});
 
-$(document).ready(function() {
-      $('#pagepiling').pagepiling({
-        direction: 'vertical',
-        anchors: ['page1', 'page2', 'page3', 'page4'],
-        menu: '#menu',
-        sectionsColor: ['white', '#ee005a', '#2C3E50', '#39C'],
-        navigation: {
-          'position': 'right',
-          'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4']
-        },
-        onLeave: function (index, nextIndex, direction) {
-          if (nextIndex == 4) {
-            $('#pp-nav').fadeOut();
-          }
-          if (index == 4) {
-            $('#pp-nav').fadeIn();
-          }
-        }
-      });
-    });
+// wait for PR https://github.com/knsv/mermaid/pull/577 to get merged in to use pagepiling with mermaid
+// $(document).ready(function() {
+//   $('#pagepiling').pagepiling({
+//     direction: 'vertical',
+//     anchors: ['page1', 'page2', 'page3', 'page4'],
+//     menu: '#menu',
+//     sectionsColor: ['white', '#ee005a', '#2C3E50', '#39C'],
+//     navigation: {
+//       'position': 'right',
+//       'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4']
+//     },
+//     onLeave: function (index, nextIndex, direction) {
+//       if (nextIndex == 4) {
+//         $('#pp-nav').fadeOut();
+//       }
+//       if (index == 4) {
+//         $('#pp-nav').fadeIn();
+//       }
+//     }
+//   });
+// });
